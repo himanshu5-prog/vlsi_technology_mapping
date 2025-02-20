0d42f9eb8f3dfe40a023f6c44ff533603265a10f
Himanshu Yadav <himanshu07319@gmail.com>
2025-02-19 16:36:46 -0800

    Add Gate class

---
+++ src/Gate/gate.cc
@@ -0,0 +1,123 @@
+#include "gate.hh"
+//typedef std::shared_ptr<Gate> GatePtr;
+typedef Gate* GatePtr;
+std :: string getStringGateType(GateType g){
+
+    std :: string strGate = "NA";
+    switch (g){
+        case INPUT: 
+            strGate = "INPUT";
+            break;
+        case OUTPUT:
+            strGate = "OUTPUT";
+            break;
+        case NOT:
+            strGate = "NOT";
+            break;
+        case AND2:
+            strGate = "AND2";
+            break;
+        case OR2:
+            strGate = "OR2";
+            break;
+        case NOR2:
+            strGate = "NOR2";
+            break;
+        case AOI21:
+            strGate = "AOI21";
+            break;
+        case NAND2:
+            strGate = "NAND2";
+            break;
+        case AOI22:
+            strGate = "AOI22";
+            break;
+        default:
+            strGate = "NA";
+    }
+
+    return strGate;
+}
+
+Gate ::
+Gate (){
+    gateType = NA;
+    gateId = -1;
+    inputCount = 0;
+}
+
+Gate :: 
+Gate(GateType g, int id, GatePtr outGate, int ipCount, std :: vector <GatePtr> inGate){
+    gateType = g;
+    gateId = id;
+    inputCount = ipCount;
+    inputGate = inGate;
+
+}
+
+Gate :: ~Gate(){
+    for (auto elem: inputGate){
+        delete elem;
+    }
+}
+
+void Gate :: printGate(){
+    std :: cout << "Printing Gate:\n";
+    std :: cout << "GateType: " << getStringGateType(gateType) << "\n";
+    std :: cout << "Gate id: " << gateId << "\n";
+    
+
+    if (inputCount > 0){
+        std :: cout << "Input count: " << inputCount << "(" << inputGate.size() << ")"<< "\n";
+        std :: cout << "Input gate ID: ";
+        for (int elem: inputGateId){
+            std :: cout << elem << " ";
+        }
+        std :: cout << "\n";
+    }
+    
+
+    for (auto elem: inputGate){
+
+       elem->printGate();
+        
+    }
+
+}
+
+// Get method
+GateType Gate:: getGateType(){
+    return gateType;
+}
+
+int Gate :: getGateId(){
+    return gateId;
+}
+
+
+int Gate :: getInputCount(){
+    return inputCount;
+}
+
+std :: vector <GatePtr> 
+Gate :: getInputGate(){
+    return inputGate;
+}
+
+// set method
+void Gate :: setGateType(GateType g){
+    gateType = g;
+}
+
+void Gate :: setGateId (int id){
+    gateId = id;
+}
+
+
+void Gate :: setInputCount (int c){
+    inputCount = c;
+}
+
+void Gate :: setInputGate(std :: vector < GatePtr> v) {
+    inputGate = v;
+}
---
+++ src/Gate/gate.hh
@@ -0,0 +1,107 @@
+#ifndef __GATE_HH__
+#define __GATE_HH__
+#include <iostream>
+#include <vector>
+#include <memory>
+
+
+enum GateType {
+    INPUT,
+    OUTPUT,
+    NOT,
+    AND2,
+    NAND2,
+    OR2,
+    NOR2,
+    AOI21,
+    AOI22,
+    NA
+
+};
+
+std :: string getStringGateType (GateType g);
+
+struct GateStats {
+    int outCount;
+    int inCount;
+    int notCount;
+    int nandCount;
+
+    GateStats(){
+        outCount = 0;
+        inCount = 0;
+        notCount = 0;
+        nandCount = 0;
+    }
+};
+
+/*
+Each gate will have 1 output and multiple inputs.
+*/
+
+class Gate {
+    typedef std::shared_ptr<Gate> SharedGatePtr;
+    typedef std::weak_ptr<Gate> WeakGatePtr;
+
+    typedef Gate* GatePtr;
+
+
+    GateType gateType;
+    int gateId;
+    
+    // Output side
+    //GatePtr outputGate;
+    //int outputGateId;
+
+    int inputCount;
+    std :: vector <GatePtr> inputGate;
+    std :: vector <int> inputGateId;
+
+    public:
+        Gate();
+        Gate(GateType g, int id, GatePtr gPtr, int ipCount, std :: vector<GatePtr> inGate);
+        ~Gate();
+        // Get methods
+
+        // Gate type
+        GateType getGateType();
+
+        // Gate ID
+        int getGateId();
+
+        // Fanout gate pointer
+        //GatePtr getOutputGate();
+        //int getOutputGateId(){
+        //    return outputGateId;
+        //}
+
+        // Input side 
+        int getInputCount();
+        std :: vector <GatePtr> getInputGate();
+        std :: vector <int> getinputGateId;
+
+        // Set methods
+        void setGateType(GateType g);
+        void setGateId (int id);
+
+        void setInputCount(int c);
+        void setInputGate( std :: vector < GatePtr > v);
+        void addInputGate(GatePtr g){
+            inputGate.push_back(g);
+            inputGateId.push_back(g->getGateId());
+        }
+        void addInputGateId(int i){
+            inputGateId.push_back(i);
+        }
+
+        //void setOutputGate (GatePtr g);
+        //void setOutputGateId(int i){
+        //     outputGateId = i;
+        //}
+
+
+        void printGate();
+
+
+};
+#endif
