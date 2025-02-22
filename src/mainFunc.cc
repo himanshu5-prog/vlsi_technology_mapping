efb206b788a678098b1badb27081022f99a56485
Himanshu Yadav <himanshu07319@gmail.com>
2025-02-21 20:17:59 -0800

    Add command line argument and did some refactoring

--- src/mainFunc.cc
+++ src/mainFunc.cc
@@ -1,11 +1,34 @@
 #include <iostream>
 #include "TreeMatch/treeMatch.hh"
-int main(){
-  
+int main(int argc, char* argv[]){
+    
+    // Handling command line argument
+    assert(argc <= 2);
+    bool debugMode = false;
+    if (argc == 2){
+        if (strcmp(argv[1], "-v") == 0){
+            debugMode = true;
+            std :: cout << "Debug mode is enabled\n";
+        } else if (strcmp(argv[1], "-h") == 0){
+            std :: cout << "Run this command to run without debug mode enabled: ./bin/techmap\n";
+            std :: cout << "Run this command to run with debug mode enabled: ./bin/techmap -v\n";
+            return 1;
+        }
+        else {
+            std :: cout << "Argument not identified: "<< argv[1] << "Only vavlid argument: -v. Use ./bin/techmap -h for details \n"; 
+            return 2;
+        }
+    }
+    //--------------------------------------------------------
+
+
     TreeMatch techMap;
 
-    techMap.init();
-    techMap.doMatching();
+    if (debugMode) {
+        techMap.setDebugMode();
+    }
+    
+    techMap.run();
     techMap.print();
     
     return 0;
