d5906c80b4387c060b1b9568e6e92413ee354db9
Himanshu Yadav <himanshu07319@gmail.com>
2025-02-24 19:49:23 -0800

    Make typedefs public

--- src/Gate/gate.hh
+++ src/Gate/gate.hh
@@ -41,10 +41,7 @@ Each gate will have 1 output and multiple inputs.
 */
 
 class Gate {
-    typedef std::shared_ptr<Gate> SharedGatePtr;
-    typedef std::weak_ptr<Gate> WeakGatePtr;
-
-    typedef Gate* GatePtr;
+  
 
 
     GateType gateType;
@@ -60,6 +57,10 @@ class Gate {
     std :: vector <int> inputGateId;
 
     public:
+        typedef std::shared_ptr<Gate> SharedGatePtr;
+        typedef std::weak_ptr<Gate> WeakGatePtr;
+
+        typedef Gate* GatePtr;
         Gate();
         Gate(GateType g, int id, int ipCount, std :: vector<GatePtr> inGate);
         ~Gate();
